18dd2ec78ff7b0d407ad0c0dafe3519fd76329cb
Robin Malhotra <me@rmalhotra.com>
2020-09-01 22:23:33 +0100

    pure functions!
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- gatsby/src/components/MarkdownRenderer.js
+++ gatsby/src/components/MarkdownRenderer.js
@@ -3,14 +3,12 @@ import remark from "remark"
 import remark2react from "remark-react"
 import externalLinks from "remark-external-links"
 
-class MarkdownRenderer extends React.Component {
-  render() {
-    let html = remark()
-      .use(externalLinks, { target: "_blank" })
-      .use(remark2react)
-      .processSync(this.props.text).result
-    return html
-  }
+const MarkdownRenderer = ({ text }) => {
+  let html = remark()
+    .use(externalLinks, { target: "_blank" })
+    .use(remark2react)
+    .processSync(text).result
+  return html
 }
 
 export default MarkdownRenderer
