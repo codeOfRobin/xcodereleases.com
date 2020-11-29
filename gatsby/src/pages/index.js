28c085dcda287e0eb4855e48e157009a315d3083
Robin Malhotra <me@rmalhotra.com>
2020-11-29 22:14:08 +0000

    Update json
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import "../styles/styles.css"
 import Header from "../components/Header"
 import Table from "../components/Table"
 import Footer from "../components/Footer"
